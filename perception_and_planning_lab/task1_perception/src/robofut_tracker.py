8e3746e76c1071004627929bd97774ecc8a169b4
97hackbrian <brayandurantoconas@gmail.com>
2025-11-25 17:22:06 -0400

    feat: Introduce `ball_tracker` module and integrate `KalmanTracker` into `robofut_tracker.py`.

--- perception_and_planning_lab/task1_perception/src/robofut_tracker.py
+++ perception_and_planning_lab/task1_perception/src/robofut_tracker.py
@@ -1,3 +1,12 @@
 import cv2
 import numpy as np
 import argparse
+import sys
+
+sys.path.append('/home/hackbrian/Documents/gits/RoboticsLab_DRM/perception_and_planning_lab/python_scripts')
+
+sys.path.append('RoboticsLab_DRM/perception_and_planning_lab/python_scripts')
+import ball_tracker
+
+if __name__ == '__main__':
+    kalaman=ball_tracker.KalmanTracker()
